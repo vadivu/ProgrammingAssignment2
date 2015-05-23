a6d07096cfee965d6f8b46e1975f9b9d29d75fc0
vadivu <vadivukar@yahoo.com>
2015-05-23 11:40:10 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,9 @@
+#The first function, makeVector creates a special "vector", which is really a list containing a function to
+
+#    set the value of the vector
+#    get the value of the vector
+#    set the value of the mean
+#    get the value of the mean
 makeCacheMatrix <- function(x = matrix()) {
         
         inv = NULL
